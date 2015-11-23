190491175f1a279ceabc05b35e1f927e771f0b11
Keisuke Imura <keisuke@keisuke-imura.com>
2015-11-23 12:15:49 +0900

    first commit

---
+++ wp-kpi-dashboard.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Wp KPI Dashboard
+Version: 0.1-alpha
+Description: Display your kpi and current stats on dashboard.
+Author: Keisuke Imura
+Author URI: https://funteractive.co.jp/
+Plugin URI: https://funteractive.co.jp/
+Text Domain: wp-kpi-dashboard
+Domain Path: /languages
+License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
